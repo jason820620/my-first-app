d216a4ab9c3b0b6487e4d9312ecdf961acf0afac
Jason Wong <Jason Wong>
2017-05-25 01:29:15 +0800

    updated script for custom element binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,22 @@ import { Component } from '@angular/core';
   // `]
 })
 export class AppComponent {
-  title = 'My First App';
+  // title = 'My First App';
+    serverElements = [{ type: 'server', name: 'TesrServer', content: 'Just a test'}];
+
+    onServerAdded(serverData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+        type: 'server',
+        name: serverData.serverName,
+        content: serverData.serverContent
+      })
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    })
+  }
 }
